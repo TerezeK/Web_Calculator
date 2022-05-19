211592816fd9e6e22761843dfc8ec94c1d7a2aa9
TerezeK <98523324+TerezeK@users.noreply.github.com>
2022-05-19 12:23:01 +0300

    Updated to teacher's file

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-function SumCalculator(){
-    let firstnumber = document.getElementById("number1").value;
-    let secondnumber = document.getElementById("number2").value;
-    let sum = Number(firstnumber) + Number(secondnumber);
-    alert("Your number is: " + sum)
+let btns = document.querySelectorAll(".num-button");
+let allBtns = document.querySelectorAll(".button");
+let resultBox = document.querySelector("#result-box");
+let clearBtn = document.querySelector('#clear');
+
+let total = document.querySelector("#total");
+
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
+
+// For Number Inputs
+btnSpread.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    // Inner Values for calculator
+
+    if (resultBox.innerHTML == "0") {
+      resultBox.innerHTML = "";
+    }
+
+    let value = btns[i].innerHTML;
+    resultBox.innerHTML += value;
+  });
+});
+
+// Function to evalute Strings
+function evaluate(fn) {
+    return new Function('return ' + fn)();
 }
 
-function SwitchMode(){
-    let element = document.body;
-    let button = document.getElementById("dmbutton");
-        element.classList.toggle("dark-mode");
-    if (button.value == "Dark mode"){
-        button.value="Light mode"
-    } else {
-        button.value="Dark mode"
-    }
-}
+// To calculate All Input
+total.addEventListener('click', ()=> {
+let allInputs = resultBox.innerHTML;
 
-function Calculate(){
-    let firstnumber = document.getElementById("number1").value;
-    let secondnumber = document.getElementById("number2").value;
-    let operation = document.getElementById("Operation").value;
-    firstnumber = parseFloat(firstnumber);
-    secondnumber = parseFloat(secondnumber);
-    if (operation == "+"){
-        result = firstnumber + secondnumber
-    } else if (operation == "-"){
-        result = firstnumber - secondnumber
-    } else if (operation == "x"){
-        result = firstnumber * secondnumber
-    } else if (operation == "/"){
-        result = firstnumber / secondnumber
-    } else {
-        result = "Invalid Input"
-    }
-    document.getElementById("result").value = result;
-}
+resultBox.innerHTML = evaluate(allInputs);
+
+console.log(evaluate(allInputs));
+})
+
+// Clear all Inputs
+clearBtn.addEventListener('click', ()=> {
+    resultBox.innerHTML = "0";
+})
